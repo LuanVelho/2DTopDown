6b7685160216363ae8579f07aaf210d2bce346f0
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2025-08-13 10:30:54 -0300

    Versão 2
    Adicionei armadilha funcional, vida, moedas e Audio da moeda quando você pega

--- Assets/Scripts/ItemControl.cs
+++ Assets/Scripts/ItemControl.cs
@@ -4,9 +4,13 @@ public class ItemControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int coins;
+    public int health;
+    public int maxHealth = 100;
+    AudioSource audioSource;
     void Start()
     {
-        
+        health = maxHealth;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -15,9 +19,19 @@ public class ItemControl : MonoBehaviour
         Collider2D collision = Physics2D.OverlapCircle(transform.position, 0.1f, 128);
         if (collision)
         {
-            coins++;
-
-            Destroy(collision.gameObject);
+            if (collision.CompareTag("Coin"))
+            {
+                coins++;
+                print($"You have {coins} coins");
+                audioSource.Play();
+                Destroy(collision.gameObject);
+            }
+            if (collision.CompareTag("Trap"))
+            {
+                health -= 10;
+                print($"Your health: {health}");
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
